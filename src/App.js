6d92648b05d39d17effbd7b4dc506f639d3cb545
Kendrick <drewskenny@gmail.com>
2019-07-06 18:21:47 -0400

    Site Scaffold and API Request Start.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo-endless.svg';
+import MyComponent from './ajax.js';
 import './App.css';
+import './index.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+        <img src={logo} className="App-logo" alt="logo" />       
       </header>
+      <main className="main-container">
+        <section className="main-content--container">
+          <h3> first line</h3>
+          <h2> second line p1 </h2>
+          <h2> second line p2 </h2>
+          <p> final test </p>
+          <button>Get Started</button>
+        </section>
+      </main>
+      <footer className="how-it-works">
+        <MyComponent/>
+      </footer>
     </div>
   );
 }
-
 export default App;
