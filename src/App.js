b3a622b374c059ab047fa8d606def96a1bc0208a
Kendrick <drewskenny@gmail.com>
2019-07-07 19:15:40 -0400

    Fully Responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './assets/logo-endless.svg';
-import MyComponent from './ajax.js';
+import APIRequest from './apiRequest.js';
 import './App.css';
 import './index.css';
 
@@ -13,16 +13,19 @@ function App() {
       </header>
       <main className="main-container">
         <section className="main-content--container">
-          <h3> first line</h3>
-          <h2> second line p1 </h2>
-          <h2> second line p2 </h2>
-          <p> final test </p>
-          <button>Get Started</button>
+          <div className="section-column">
+            <h3 className="shadowed">New Games & Accessories</h3>
+            <h2 className="shadowed">Monthly packages.</h2>
+            <h2 className="shadowed">Excitement delivered daily.</h2>
+            <p className="shadowed">What’s the best way to shop for the latest video games and peripherals? How about never shopping at all? You’ll get new stuff on your doorstep — every month.</p>
+            <button>GET STARTED</button>
+          </div>
         </section>
       </main>
-      <footer className="how-it-works">
-        <MyComponent/>
-      </footer>
+      <section className="how-it-works">        
+        <p className="section-title">How It Works</p>        
+        <APIRequest/>
+      </section>
     </div>
   );
 }
--- src/apiRequest.js
+++ src/apiRequest.js
@@ -15,7 +15,7 @@ class MyComponent extends React.Component {
       fetch("https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge")
         .then(res => res.json())
         .then(
-          (result) => {
+          (result) => {            
             this.setState({
               isLoaded: true,
               items: result
@@ -32,21 +32,41 @@ class MyComponent extends React.Component {
           }
         )
     }
+    
   
     render() {
       const { error, isLoaded, items } = this.state;
+      const finalArray = items.sort(function (a, b) {
+        return a.stepNumber - b.stepNumber;
+      }).map(item => {
+            const finalItem = {};
+            const versionDates = item.versionContent
+            
+            const sortByEffectiveDate = versionDates.sort(function (a,b) {               
+                return Date.parse(b.effectiveDate) - Date.parse(a.effectiveDate); 
+            });              
+            finalItem.stepNumber = item.stepNumber;
+            finalItem.title = sortByEffectiveDate[0].title;
+            finalItem.body = sortByEffectiveDate[0].body;            
+        return finalItem;
+      });
+    console.log(items);
+      console.log(finalArray);      
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
         return <div>Loading...</div>;
       } else {
         return (            
-          <div>
-            {items.map((item) => (
-              <div key={item.title}>
-                {item.body} {item.effectiveDate}
+          <div className="api-row">
+            { finalArray.map((item) => (
+                <div className="api-container" key={item.stepNumber}>
+                    <p className="api-stepNumber">0{item.stepNumber}</p>
+                    <hr className="api-separator"></hr>
+                    <p className="api-title">{item.title.toUpperCase()}</p>
+                    <p className="api-body">{item.body}</p>
                 </div>
-            ))}
+            )) }
           </div>
         );
       }
